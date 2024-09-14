db7bd17b1ffc2e289e8e49651dcece59f345bc95
KarandaAR <karandaalexandr@gmail.com>
2024-09-14 23:34:07 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.KarandaAR.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tyuiu.KarandaAR.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Alexandr";
+            int age = 18;
+
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
