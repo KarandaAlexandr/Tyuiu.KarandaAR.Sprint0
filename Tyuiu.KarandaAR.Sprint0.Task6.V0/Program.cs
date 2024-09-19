6e418ef5efd893b1a2caeb5c41c602e9f4a973a8
KarandaAR <karandaalexandr@gmail.com>
2024-09-19 21:23:26 +0500

    таск6

--- Tyuiu.KarandaAR.Sprint0.Task6.V0/Program.cs
+++ Tyuiu.KarandaAR.Sprint0.Task6.V0/Program.cs
@@ -9,8 +9,8 @@ namespace Tyuiu.KarandaAR.Sprint0.Task6.V0
             int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
 
             Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
-            Console.WriteLine("Сумма элементов массива = " + DataService.SubstrationArray(arraynums));
-            Console.WriteLine("Сумма элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstrationArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
             
             Console.ReadKey();
         }
